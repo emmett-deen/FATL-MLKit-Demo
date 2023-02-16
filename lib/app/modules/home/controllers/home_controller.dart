b7c6ab3bd6cbf4b8656240bd9436d01bb7cdc67b
Emmett Deen <emmett.deen@kickasscoding.com>
2023-02-16 10:52:13 -0500

    Added pose detection logic

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,87 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 class HomeController extends GetxController {
   Rx<CameraController?> camController = Rx<CameraController?>(null);
+  RxList<Pose> poses = RxList<Pose>([]);
+
+  @override
+  void onInit() async {
+    super.onInit();
+
+    // Initialize the camera controller
+    camController.value = await _initCamera();
+
+    // Initialize the pose detector
+    var poseDetector = _initPoseDetector();
+
+    // Start the pose detection
+    _startPoseDetection(poseDetector);
+  }
+
+  Future<CameraController> _initCamera() async {
+    // Get the list of available cameras
+    final cameras = await availableCameras();
+
+    // Initialize the camera controller
+    // Index 1 is the front facing camera
+    var controller = CameraController(cameras[1], ResolutionPreset.max);
+    await controller.initialize();
+    return controller;
+  }
+
+  PoseDetector _initPoseDetector() {
+    final options = PoseDetectorOptions(
+        model: PoseDetectionModel.accurate, mode: PoseDetectionMode.stream);
+    return PoseDetector(options: options);
+  }
+
+  void _startPoseDetection(PoseDetector poseDetector) async {
+    // Start camera streaming
+    await camController.value!.startImageStream((image) async {
+      // Detect poses
+      final newPoses = await poseDetector
+          .processImage(_createInputImageFromCameraImage(image));
+
+      // Do something with the poses
+      poses.value = newPoses;
+    });
+  }
+
+  InputImage _createInputImageFromCameraImage(CameraImage cameraImage) {
+    final Size imageSize =
+        Size(cameraImage.width.toDouble(), cameraImage.height.toDouble());
+
+    final InputImageRotation? imageRotation =
+        InputImageRotationValue.fromRawValue(0);
+
+    final InputImageFormat? inputImageFormat =
+        InputImageFormatValue.fromRawValue(cameraImage.format.raw);
+
+    final planeData = cameraImage.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: inputImageFormat!,
+      planeData: planeData,
+    );
+
+    final inputImage = InputImage.fromBytes(
+      bytes: cameraImage.planes[0].bytes,
+      inputImageData: inputImageData,
+    );
+
+    return inputImage;
+  }
 }
